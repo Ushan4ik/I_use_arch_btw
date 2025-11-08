81f74483565d7c42a14039736af6a819d83c198f
Ushan4ik <314sanchez@gmail.com>
2025-11-08 12:32:23 +0300

    sin cos change fun choosing idea

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,5 +1,7 @@
 package ru.tbank.education.school.lesson1
-
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 enum class Operation3val(val symbols: List<String>, val operationfun: (Double, Double) -> Double?) {
     MULT(listOf("*", "multiply"), { a, b -> a * b }),
     PLUS(listOf("+", "plus"), { a, b -> a + b }),
@@ -8,6 +10,16 @@ enum class Operation3val(val symbols: List<String>, val operationfun: (Double, D
         { a, b -> if(b!=0.0){a / b} else{   null }});
     companion object {
         fun from(symbol: String): Operation3val? {
+            return entries.find { it.symbols.contains(symbol) }
+        }
+    }
+}
+enum class Operation2val(val symbols: List<String>, val operationfun: (Double) -> Double?) {
+    SIN(listOf("SIN","sin","Sin","S","s"), { a-> sin(Math.toRadians(a))  }),
+    COS(listOf("COS","cos","Cos","C","c"), { a-> cos(Math.toRadians(a))  });
+
+    companion object {
+        fun from(symbol: String): Operation2val? {
             return entries.find { it.symbols.contains(symbol) }
         }
     }
@@ -23,6 +35,10 @@ fun String.calculate(): Double? {
 
             val operation = Operation3val.from(operationSymbol) ?: return null
             return operation.operationfun(a, b)}
+        2->{val a = parts[1].toDoubleOrNull() ?: return null
+            val operationSymbol = parts[0]
+            val operation = Operation2val.from(operationSymbol) ?: return null
+            return operation.operationfun(a)}
 
     }
     return null
