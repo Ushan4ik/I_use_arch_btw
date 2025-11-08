a5594fc94a9a1b013f47270118ff7fc0423678eb
Ushan4ik <314sanchez@gmail.com>
2025-11-08 12:23:59 +0300

    first commit ever

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,13 +1,13 @@
 package ru.tbank.education.school.lesson1
 
-enum class Operation(val symbols: List<String>, val operationfun: (Double, Double) -> Double) {
+enum class Operation3val(val symbols: List<String>, val operationfun: (Double, Double) -> Double?) {
     MULT(listOf("*", "multiply"), { a, b -> a * b }),
     PLUS(listOf("+", "plus"), { a, b -> a + b }),
     MINUS(listOf("-", "minus"), { a, b -> a - b }),
-    DIVIDE(listOf("/", "d", "divide"), { a, b -> a / b });
-
+    DIVIDE(listOf("/", "d", "divide"),
+        { a, b -> if(b!=0.0){a / b} else{   null }});
     companion object {
-        fun from(symbol: String): Operation? {
+        fun from(symbol: String): Operation3val? {
             return entries.find { it.symbols.contains(symbol) }
         }
     }
@@ -15,21 +15,21 @@ enum class Operation(val symbols: List<String>, val operationfun: (Double, Doubl
 
 fun String.calculate(): Double? {
     val parts = this.split(" ")
-    if (parts.size != 3) {
-        return null
+    when (parts.size )
+    {
+        3->{val a = parts[0].toDoubleOrNull() ?: return null
+            val operationSymbol = parts[1]
+            val b = parts[2].toDoubleOrNull() ?:return null
+
+            val operation = Operation3val.from(operationSymbol) ?: return null
+            return operation.operationfun(a, b)}
+
     }
-
-    val a = parts[0].toDoubleOrNull() ?: return null
-    val operationSymbol = parts[1]
-    val b = parts[2].toDoubleOrNull() ?: return null
-
-    val operation = Operation.from(operationSymbol) ?: return null
-
-    return operation.operationfun(a, b)
+    return null
 }
 
 fun main() {
-    val s = "2 + 3"
+    val s = "2 / 0"
     println(s.calculate())
     val z = readln()
     println(z.calculate())
