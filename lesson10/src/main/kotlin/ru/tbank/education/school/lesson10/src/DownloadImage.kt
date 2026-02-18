6299898001e6d4ce97adf213bbe6a11434c71c1e
ushan4ik <314sanchez@gmail.com>
2026-02-19 00:43:39 +0300

    Merge branch 'Stasik371:master' into master

--- lesson10/src/main/kotlin/ru/tbank/education/school/lesson10/src/DownloadImage.kt
+++ lesson10/src/main/kotlin/ru/tbank/education/school/lesson10/src/DownloadImage.kt
@@ -1,15 +1,60 @@
 package ru.tbank.education.school.lesson10.src
-
-import kotlinx.coroutines.runBlocking  // Add this import
-
-data class DownloadStats(
-    val url: String,
-    val bytesDownloaded: Long,
-    val timeMs: Long
-)
-
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import java.io.File
+import java.util.concurrent.Executors
+import java.util.concurrent.Callable
+import java.net.URL
+import java.util.concurrent.TimeUnit
 object ImageDownloader {
-    fun run(urls: List<String>, outputDir: String): DownloadStats = runBlocking {
-        TODO() // You need to implement this function
+    suspend fun run(urls: List<String>, dir: String) = withContext(Dispatchers.IO) {
+        File(dir).mkdirs()
+
+        /* val results = urls.mapIndexed { i, url ->
+            try {
+                val file = File(dir, "img_${i+1}.jpg")
+                URL(url).openStream().use { it.copyTo(file.outputStream()) }
+                println("YES!!! Загружено ${i+1}/${urls.size}")
+                Result.success(file)
+            } catch (e: Exception) {
+                println("NO!!!! Ошибка ${i+1}: ${e.message}")
+                Result.failure(e)
+            }
+        }
+
+        println("\nУспешно: ${results.count { it.isSuccess }} из ${urls.size}")
+    }*/
+
+        val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+
+        val tasks = urls.mapIndexed { index, url ->
+            Callable {
+                try {
+                    val file = File(dir, "img_${index + 1}.jpg")
+                    URL(url).openStream().use { it.copyTo(file.outputStream()) }
+                    println("YES!!! Загружено ${index + 1}/${urls.size}")
+                    Result.success(file)
+                } catch (e: Exception) {
+                    println("NO!!!! Ошибка ${index + 1}: ${e.message}")
+                    Result.failure(e)
+                }
+            }
+        }
+
+        val futures = executor.invokeAll(tasks)
+        val results = futures.map { it.get() }
+
+        executor.shutdown()
+        executor.awaitTermination(1, TimeUnit.MINUTES)
+
+        println("\nУспешно: ${results.count { it.isSuccess }} из ${urls.size}")
+
     }
+
+    }
+
+// Запуск:
+fun main() = runBlocking {
+    val urls = List(10) { "https://picsum.photos/200/300?random=$it" }
+    ImageDownloader.run(urls, "downloads")
 }
